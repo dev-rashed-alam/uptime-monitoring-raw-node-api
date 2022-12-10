03a2745aa86f7ce9387b542d4deecaa824e9afd7
dev-rashed-alam <dev.rashedalam@gmail.com>
2022-12-10 17:48:58 +0600

    Added file create, read, update, delete functionality

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 const app = {};
+
+// data.create('test', 'testFile', { name: 'Rashed Alam', age: 23 }, (err) => {
+//     console.log(err);
+// });
+
+// data.read('test', 'testFile', (err, result) => {
+//     console.log(err, result);
+// });
+
+// data.update('test', 'testFile', { name: 'Rashed', age: 24 }, (err) => {
+//     console.log(err);
+// });
+
+// data.delete('test', 'testFile', (err) => {
+//     console.log(err);
+// });
 
 app.createServer = () => {
     const server = http.createServer(app.handleRequest);
